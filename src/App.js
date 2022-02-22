42fc46fae9cf4dc6dbe0644b15293620f0be2cf4
hsgittest <hsjeon@npcsystem.co.kr>
2022-02-22 12:07:23 +0900

    route path 변경

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ function App() {
     <Router>
       <Routes>
         <Route
-          path={process.env.PUBLIC_URL + "/movie/:id"}
+          // path={process.env.PUBLIC_URL + "/movie/:id"}
+          basename={process.env.PUBLIC_URL}
+          path="/movie/:id"
           element={<Detail />}
         ></Route>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Home />}></Route>
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path="/"
+          element={<Home />}
+        ></Route>
       </Routes>
     </Router>
   );
